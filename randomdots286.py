9fbddd3db7fca5aebd94fceb63fa1e077152f581
Themis Karaminis <themkar@Amsyss-MacBook-Pro.local>
2016-04-20 18:25:33 +0100

    286close

--- randomdots286.py
+++ randomdots286.py
@@ -362,7 +362,7 @@ class Patterns:
       print 'Cophenetic correlation coefficient of clustering (the closer to 1, the better):', c
 
       # calculate full dendrogram
-      fig = plt.figure(figsize=(20, 10))
+      fig = plt.figure(figsize=(400, 10))
       ax = fig.add_subplot(111)
       plt.title('Hierarchical Clustering Dendrogram')
       plt.xlabel(x_label)
@@ -370,7 +370,7 @@ class Patterns:
       sch.dendrogram(
           Z,
           leaf_rotation=90,  # rotates the x axis labels
-          leaf_font_size=5,  # font size for the x axis labels
+          leaf_font_size= 7,  # font size for the x axis labels
           labels = self.labels
 
       )
