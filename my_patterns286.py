4c05332402e398499036b57b78678b0f4cfc775f
Themis Karaminis <themkar@Amsyss-MacBook-Pro.local>
2016-04-20 16:39:23 +0100

    286rd

--- my_patterns286.py
+++ my_patterns286.py
@@ -4,4 +4,4 @@ from randomdots286 import *
 p = Patterns(categories = 3, items_per_category = [10, 8, 4, 1], include_prototypes = False)
 p.Dendrograms('my_patterns.pdf') #the string is part of the filename (optional), extension is optional can be pdf or png or jpg, will create file as appropriate
 p.Save('my_patterns.pkl') #uncomment to save
-#p.Load('my_patterns.pkl') #uncomment to load
+p.Load('my_patterns.pkl') #uncomment to load
